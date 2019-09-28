7b21627a66317a5ee7ac19b37483253464025a66
Brian Hanifin <code@brianhanifin.com>
2019-09-27 20:53:44 -0700

    updates + tweaks

---
+++ custom_components/browser_mod/const.py
@@ -0,0 +1,17 @@
+DOMAIN = "browser_mod"
+
+FRONTEND_SCRIPT_URL = "/browser_mod.js"
+
+DATA_EXTRA_MODULE_URL = 'frontend_extra_module_url'
+
+DATA_DEVICES = "devices"
+DATA_ALIASES = "aliases"
+DATA_ADDERS = "adders"
+DATA_CONFIG = "config"
+
+CONFIG_DEVICES = "devices"
+
+WS_ROOT = DOMAIN
+WS_CONNECT = "{}/connect".format(WS_ROOT)
+WS_UPDATE = "{}/update".format(WS_ROOT)
+WS_CAMERA = "{}/camera".format(WS_ROOT)
